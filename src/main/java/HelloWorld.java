eb5a199a631ee1c81ea57df1692b92050f677afd
rmergeee <owodow.rostislaw@gmail.com>
2024-01-28 18:06:35 +0200

    second commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("For second commit");
+        System.out.println(1+1);
     }
 }
